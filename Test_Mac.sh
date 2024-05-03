8e3e21014c82733366d7105564a5b930ad4f5a17
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-05-03 10:07:32 -0700

    inital edits

---
+++ Test_Mac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#test shell script for Mac
+#want to create a shell script to automatically download Spotify
+
+# Download URL for Spotify
+Spotify_URL="https://spotify.link/lz04YJ0r9ib?label=sp_cid%3Ae021310b-14ab-4b5c-8161-da7fba2019d0 "
+
+syslog -s -l error "Spotify - Starting Download/Install sequence."
+
+wget $Spotify_URL
+
+
+# Grant execution permissions to the script
+chmod +x script.sh
