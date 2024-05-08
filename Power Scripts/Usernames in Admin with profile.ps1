0e8dbfc9535eccef2f4f7747c8e948c2087b30cc
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-05-08 11:18:24 -0700

    Changes to syntax and formatting

--- Power Scripts/Usernames in Admin with profile.ps1
+++ Power Scripts/Usernames in Admin with profile.ps1
@@ -2,8 +2,8 @@
 $Admins=net localGroup Administrators
 
 
-#object
-$User=Get-LocalUser -Name cpsit | select FullName
+<#object
+#$User=Get-LocalUser -Name cpsit | select FullName
 
 # object | Select - ExpandPropperty Fullname
 
@@ -14,7 +14,7 @@ function In-Admins-Bool {
     else {Write-Host false}
     }
 }
-
+#>
 
 #List of user objects
 $AllUsers=Get-ChildItem -Path "C:\Users"
@@ -29,38 +29,64 @@ function Get-Profiles {
   }
 }
 
-#check if in admin
-    #true check if in users
+#Flow chart for In-Both function
+<#check if in admin
+    true check if in users
         #if true, return name of user
-    #false #move to next
-#if none, Write-Host "No Administrators with a User Profile"
+    false #move to next
+if none, Write-Host "No Administrators with a User Profile"
+#>
 
 #check if item is in given list
+#helper function for In-Both
 function In-List {
     param (
-        [object] $item,
-        [array] $list
+        [#item
+        [Parameter(Mandatory=$true)]
+        [object]
+        $item],
+
+        [# list
+        [Parameter(Mandatory=$true)]
+        [array]
+        $list]
     )
         if ($List.Contains($item)) {
             Write-Host "Item in List"
-            $Select=$item 
+            $true 
         }
         else {
             Write-Host "Item not in List"
+            $false
         }
 }
 
-#create empty array to store usernames in both admins and usernames
-$In_Admins_Usernames=new-object system.collections.arrayList
 
-#return list of usernames in both the admin and usernames list
+#return list objects in both $list1 and $list2
+# if none, returns nothing
 function In-Both {
-    foreach ($name in $Admins) {
-        if (In-List -item $name -list $Usernames)
+    param (
+        [# list1
+        [Parameter(Mandatory=$true)]
+        [arrayList]
+        $list1],
+
+        [#list2
+        [Parameter(Mandatory=$true)]
+        [arrayList]
+        $list2]
+    )
+    #list to store objects in both $list1 and $list2
+    $List_Both=new-object system.collections.arrayList
+    foreach ($object in $list1) {
+        if (In-List -item $object -list $list2)
             {
-            $In_Admins_Usernames.Add($name)
-            Write-Host $In_Admins_Usernames
-    }
-        Write-Host $In_Admins_Usernames
-   }
+                #add names in both $Admins and $Usernames into $In_Admins_Usernames
+                $List_Both.Add($name)
+        }
+        elseif ($List_Both.Length -eq 0) {
+            Write-Host "No objects in both lists."
+        }
+     } 
+        Write-Host $List_Both
 }
