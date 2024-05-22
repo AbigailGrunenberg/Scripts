19d61d96f1c70ca2c0ab439da5ee02c5a2d2ff09
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-05-22 15:28:26 -0700

    added alert message

--- Shell Scripts/Zoom_Install.sh
+++ Shell Scripts/Zoom_Install.sh
@@ -11,12 +11,6 @@ if [[ ${Applications[@]} =~ $AppName ]]
     osascript -e 'display alert "App Installation" message "Zoom app already installed on device, terminiating installation."'
     exit 0
 else   
-    Answer=$(osascript -e 'try tell app "Finder"
-            set answer to text returned of (display dislog "tester" default answer "button returned:OK")
-            end
-            end
-            activae app (path to forntmost application as text) answer' | tr '\r' ' ')
-            [[ -z "$Answer" ]] && exit
         curl -Ls -o /Users/Shared/zoom.pkg 'https://zoom.us/client/6.0.2.33403/zoomusInstallerFull.pkg'
         installer -pkg /Users/Shared/zoom.pkg -target /
         cd /Users/Shared; rm -v zoom.pkg
