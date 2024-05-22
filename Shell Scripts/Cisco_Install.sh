443b0b6f5bf5e9be34657d22184d9437f89b199a
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-05-22 16:29:41 -0700

    testing dmg download

--- Shell Scripts/Cisco_Install.sh
+++ Shell Scripts/Cisco_Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#check if zoom.us.app is already installed
+#check if Cisco Secure Endpoint is already installed
 #list of applications currently on Mac
 Applications=(/Applications/*)
 CiscoLocation="/Applications/Cisco/'Cisco AnyConnect Secure Mobility Client.app'"
@@ -21,10 +21,18 @@ dmgvol="ampmac_connector" # Name of DMG volume (obtained after mounting the file
 filename="Cisco AnyConnect Secure Mobility Client.app" # Name of file to copy
 filedestination="/Applications/" # Path to where the file should be copied to
 
-#mount dmg
-hdiutil attach /path/to/file.dmg
-
 #Retrieved from https://community.spiceworks.com/t/copying-a-file-from-a-dmg-in-bash/946768/8
+echo "--"
+echo "$(date): Downloading..."
+/usr/bin/curl -L -s -o /tmp/$dmgfile $url
+echo "$(date): Mounting..."
+/usr/bin/hdiutil attach /tmp/$dmgfile -nobrowse -quiet
+if [ $? -eq 0 ]; then
+	echo "Mount successful!"
+else
+	echo "Mount unsuccessful! Exiting installation. Please check if 'Disk Images' are being 'Allowed' within the Restriction profile."
+	exit 0
+fi
 echo "$(date): Copying..."
 cp -rf "/Volumes/$dmgvol/$filename" "$filedestination/$filename"
 chmod -R 777 "$filedestination/$filename"
@@ -34,3 +42,14 @@ echo "$(date): Unmounting..."
 /bin/sleep 10
 echo "$(date): Cleaning..."
 /bin/rm -rf /tmp/"$dmgfile"
+
+url='https://console.amp.cisco.com/install_packages/578f304b-bd79-4b41-ae5f-c058a82b5b7d/download?product=MacProduct' # URL of where the DMG is being hosted
+dmgfile="ampmac_connector.dmg" # Name of DMG file
+dmgvol="ampmac_connector" # Name of DMG volume (obtained after mounting the file)
+filename="Cisco AnyConnect Secure Mobility Client.app" # Name of file to copy
+filedestination="/Applications/" # Path to where the file should be copied to
+DMG='/Users/AG/Downloads/ampmac_connector.dmg'
+
+
+curl https://console.amp.cisco.com/install_packages/578f304b-bd79-4b41-ae5f-c058a82b5b7d/download?product=MacProduct > $DMG
+
