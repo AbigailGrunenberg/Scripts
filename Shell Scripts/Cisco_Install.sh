bd83d1ce5e0c5df26e776b3c4936cfc2c21705d5
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-05-24 12:00:00 -0700

    UNFINISHED

--- Shell Scripts/Cisco_Install.sh
+++ Shell Scripts/Cisco_Install.sh
@@ -8,38 +8,18 @@ AppName="Cisco"
 
 if [[ ${Applications[@]} =~ "/Applications/$AppName" ]]
     then
-    echo "App already installed on device, terminating installaion request."
+    osascript -e 'display alert "App Installation" message "Cisco app already installed on device, terminiating installation."'
     exit 0
 else   
-    echo "Starting installation of $AppName"
+    curl -Ls -o /Users/Shared/DropboxInstaller.dmg $URL
+    hdiutil attach /Users/Shared/DropboxInstaller.dmg
+
+    # install app
+    cp -rf /Volumes/Dropbox.app /Applications
+
+    open -a /Application/Dropbox.app
+
+    #unmount
+    hdiutil detach /Users/Shared/DropboxInstaller.dmg
+    
 fi 
-
-url="https://console.amp.cisco.com/install_packages/578f304b-bd79-4b41-ae5f-c058a82b5b7d/download?product=MacProduct" # URL of where the DMG is being hosted
-dmgfile="ampmac_connector.dmg" # Name of DMG file
-dmgvol="ampmac_connector" # Name of DMG volume (obtained after mounting the file)
-filename="Cisco AnyConnect Secure Mobility Client.app" # Name of file to copy
-filedestination="/Applications/" # Path to where the file should be copied to
-
-#Retrieved from https://community.spiceworks.com/t/copying-a-file-from-a-dmg-in-bash/946768/8
-echo "--"
-echo "$(date): Downloading..."
-/usr/bin/curl -L -s -o /tmp/$dmgfile $url
-echo "$(date): Mounting..."
-/usr/bin/hdiutil attach /tmp/$dmgfile -nobrowse -quiet
-if [ $? -eq 0 ]; then
-	echo "Mount successful!"
-else
-	echo "Mount unsuccessful! Exiting installation. Please check if 'Disk Images' are being 'Allowed' within the Restriction profile."
-	exit 0
-fi
-echo "$(date): Copying..."
-cp -rf "/Volumes/$dmgvol/$filename" "$filedestination/$filename"
-chmod -R 777 "$filedestination/$filename"
-/bin/sleep 10
-echo "$(date): Unmounting..."
-/usr/bin/hdiutil detach "$(/bin/df | /usr/bin/grep "$dmgvol" | awk '{print $1}')" -quiet
-/bin/sleep 10
-echo "$(date): Cleaning..."
-/bin/rm -rf /tmp/"$dmgfile"
-
-
