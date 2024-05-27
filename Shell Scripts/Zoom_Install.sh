0572961d0adf9c05998a747928f4b18b80d490fa
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-05-27 15:57:48 -0700

    clean up

--- Shell Scripts/Zoom_Install.sh
+++ Shell Scripts/Zoom_Install.sh
@@ -5,14 +5,15 @@
 Applications=(/Applications/*)
 ZoomLocation="/Applications/zoom.us.app"
 AppName="zoom.us.app"
+Location=/Users/Shared/zoom.pkg
 
 if [[ ${Applications[@]} =~ "/Applications/$AppName" ]]
     then
     osascript -e 'display alert "App Installation" message "Zoom app already installed on device, terminiating installation."'
     exit 0
 else   
-    curl -Ls -o /Users/Shared/zoom.pkg 'https://zoom.us/client/6.0.2.33403/zoomusInstallerFull.pkg'
-    installer -pkg /Users/Shared/zoom.pkg -target /
+    curl -Ls -o $Location 'https://zoom.us/client/6.0.2.33403/zoomusInstallerFull.pkg'
+    installer -pkg $Location -target /
     cd /Users/Shared; rm -v zoom.pkg
 fi 
 
