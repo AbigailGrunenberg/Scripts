df527dbc1a7973e571e3e7bc4478f0bc3fd40e0d
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-05-28 09:28:16 -0700

    fix RStudio installation, needs testing

--- Shell Scripts/RStudio_Install.sh
+++ Shell Scripts/RStudio_Install.sh
@@ -14,23 +14,98 @@ fi
 #check what OS is run on computer
 OS=$(sw_vers -productVersion)
 
+#parts of installation code from: 
+# https://github.com/TheJumpCloud/support/blob/master/PowerShell/JumpCloud%20Commands%20Gallery/Mac%20Commands/Application%20Installs/Mac%20-%20Install%20Chrome%20DMG.md
+
 if [[ $OS == *"12"* || $OS == *"13"* || $OS == *"14"* ]]
     then
         #continue with installation
         echo "Starting Installation"
         URL="https://download1.rstudio.org/electron/macos/RStudio-2024.04.1-748.dmg"
         Location=/Users/Shared/RStudio-2024.04.1-748.dmg
-        curl -Ls -o $Location $URL 
 
-        hdiutil attach $Location
 
-        # install app
-        cp -rf /Volumes/$AppName /Applications
+        #Create Temp Folder
+        DATE=$(date '+%Y-%m-%d-%H-%M-%S')
+        TempFolder="Download-$DATE"
+        mkdir /tmp/$TempFolder
+        cd /tmp/$TempFolder
 
-        open -a /Application/$AppName
+        # Download File into Temp Folder
+        curl -s -O "$URL"
 
-        #unmount
-        hdiutil detach $Location
+        # Capture name of Download File
+        DownloadFile="$(ls)"
+
+        echo "Downloaded $DownloadFile to /tmp/$TempFolder"
+
+        # Verifies DMG File
+        regex='\.dmg$'
+        if [[ $DownloadFile =~ $regex ]]; then
+            DMGFile="$(echo "$DownloadFile")"
+            echo "DMG File Found: $DMGFile"
+        else
+            echo "File: $DownloadFile is not a DMG"
+            rm -r /tmp/$TempFolder
+            echo "Deleted /tmp/$TempFolder"
+            exit 1
+        fi
+
+        # Mount DMG File -nobrowse prevents the volume from popping up in Finder
+        hdiutilAttach=$(hdiutil attach /tmp/$TempFolder/$DMGFile -nobrowse)
+        echo "Used hdiutil to mount $DMGFile "
+
+        regex='\/Volumes\/.*'
+        if [[ $hdiutilAttach =~ $regex ]]; then
+            DMGVolume="${BASH_REMATCH[@]}"
+            echo "Located DMG Volume: $DMGVolume"
+        else
+            echo "DMG Volume not found"
+            rm -r /tmp/$TempFolder
+            echo "Deleted /tmp/$TempFolder"
+            exit 1
+        fi
+
+        # Identify the mount point for the DMG file
+        DMGMountPoint="$(hdiutil info | grep "$DMGVolume" | awk '{ print $1 }')"
+        echo "Located DMG Mount Point: $DMGMountPoint"
+
+        # Capture name of App file
+        cd "$DMGVolume"
+        AppName="$(ls | Grep .app)"
+        cd ~
+        echo "Located App: $AppName"
+
+        DMGAppPath=$(find "$DMGVolume" -name "*.app" -depth 1)
+
+        # Copy the contents of the DMG file to /Applications/
+        # Preserves all file attributes and ACLs
+        cp -pPR "$DMGAppPath" /Applications/
+
+        err=$?
+        if [ ${err} -ne 0 ]; then
+            echo "Could not copy $DMGAppPath Error: ${err}"
+            hdiutil detach $DMGMountPoint
+            echo "Used hdiutil to detach $DMGFile from $DMGMountPoint"
+            rm -r /tmp/$TempFolder
+            echo "Deleted /tmp/$TempFolder"
+            exit 1
+        fi
+
+        echo "Copied $DMGAppPath to /Applications"
+
+        # Unmount the DMG file
+        hdiutil detach $DMGMountPoint
+        echo "Used hdiutil to detach $DMGFile from $DMGMountPoint"
+
+        err=$?
+        if [ ${err} -ne 0 ]; then
+            abort "Could not detach DMG: $DMGMountPoint Error: ${err}"
+        fi
+
+        # Remove Temp Folder and download
+        rm -r /tmp/$TempFolder
+        echo "Deleted /tmp/$TempFolder"
 
     else
        osascript -e 'display alert "App Installation" message "Installation not compatible with current OS version, terminiating installation."'
