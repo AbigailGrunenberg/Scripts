35dee2aacf2d51390b6c4a17b8ff1b192f887d1d
AbigailGrunenberg <122194442+AbigailGrunenberg@users.noreply.github.com>
2024-06-03 16:25:38 -0700

    troubleshooting dmg mounting

--- Shell Scripts/AdobeCC_Install.sh
+++ Shell Scripts/AdobeCC_Install.sh
@@ -2,3 +2,48 @@
 
 #installation script for Adobe Creative Cloud (.dmg)
 URL="https://creativecloud.adobe.com/apps/download/creative-cloud#"
+
+#!/bin/bash
+
+#referenced from: https://apple.stackexchange.com/questions/73926/is-there-a-command-to-install-a-dmg and 
+#https://github.com/TheJumpCloud/support/blob/master/PowerShell/JumpCloud%20Commands%20Gallery/Mac%20Commands/Application%20Installs/Mac%20-%20Install%20Chrome%20DMG.md
+
+#check if Creative Cloud is already installed
+#list of applications currently on Mac
+Applications=(/Applications/*)
+AppName="Adobe Creative Cloud"
+
+URL="https://creativecloud.adobe.com/apps/download/creative-cloud#"
+
+if [[ ${Applications[@]} =~ "/Applications/$AppName" ]]
+    then
+    osascript -e 'display alert "App Installation" message "Adobe Creative Cloud app already installed on device, terminiating installation."'
+    exit 0
+else   
+    #continue with installation
+    echo "Starting Installation"
+
+    #Create Temp Folder
+    DATE=$(date '+%Y-%m-%d-%H-%M-%S')
+    TempFolder="Download-$DATE"
+    Location="/tmp/$TempFolder"
+    mkdir /tmp/$TempFolder
+    cd $Location
+
+    # Download File into Temp Folder
+    curl -s -O $URL
+
+    # Capture name of Download File
+    DownloadFile="$(ls)"
+
+    echo "Downloaded $DownloadFile to $Location"
+
+    hdiutil attach /Volumes/"Creative Cloud Installer"
+
+    # install app
+    cp -rf /Volumes/"Creative Cloud Installer"/"Creative Cloud Installer.app" /Applications
+
+    hdiutil detach /Volumes/"Creative Cloud Installer"
+
+    #remove temp folder
+    rm -r $Location
--- Shell Scripts/Dropbox_Install.sh
+++ Shell Scripts/Dropbox_Install.sh
@@ -18,89 +18,31 @@ else
     #continue with installation
     echo "Starting Installation"
     URL="https://www.dropbox.com/download?os=mac"
-    Location=/tmp/$TempFolder
 
+    #continue with installation
+    echo "Starting Installation"
 
     #Create Temp Folder
     DATE=$(date '+%Y-%m-%d-%H-%M-%S')
     TempFolder="Download-$DATE"
-    mkdir $Location
+    Location="/tmp/$TempFolder"
+    mkdir /tmp/$TempFolder
     cd $Location
 
     # Download File into Temp Folder
-    curl -s -O "$URL"
+    curl -s -o /Users/Shared/DropboxInstaller.dmg $URL
 
     # Capture name of Download File
     DownloadFile="$(ls)"
 
     echo "Downloaded $DownloadFile to $Location"
 
-    # Verifies DMG File
-    regex='\.dmg$'
-    if [[ $DownloadFile =~ $regex ]]; then
-        DMGFile="$(echo "$DownloadFile")"
-        echo "DMG File Found: $DMGFile"
-    else
-        echo "File: $DownloadFile is not a DMG"
-        rm -r $Location
-        echo "Deleted $Location"
-        exit 1
-    fi
+    hdiutil attach /Volumes/"Creative Cloud Installer"
 
-    # Mount DMG File -nobrowse prevents the volume from popping up in Finder
-    hdiutilAttach=$(hdiutil attach $Location/$DMGFile -nobrowse)
-    echo "Used hdiutil to mount $DMGFile "
+    # install app
+    cp -rf /Volumes/"Creative Cloud Installer"/"Creative Cloud Installer.app" /Applications
 
-    regex='\/Volumes\/.*'
-    if [[ $hdiutilAttach =~ $regex ]]; then
-        DMGVolume="${BASH_REMATCH[@]}"
-        echo "Located DMG Volume: $DMGVolume"
-    else
-        echo "DMG Volume not found"
-        rm -r $Location
-        echo "Deleted $Location"
-        exit 1
-    fi
+    hdiutil detach /Volumes/"Creative Cloud Installer"
 
-    # Identify the mount point for the DMG file
-    DMGMountPoint="$(hdiutil info | grep "$DMGVolume" | awk '{ print $1 }')"
-    echo "Located DMG Mount Point: $DMGMountPoint"
-
-    # Capture name of App file
-    cd "$DMGVolume"
-    AppName="$(ls | Grep .app)"
-    cd ~
-    echo "Located App: $AppName"
-
-    DMGAppPath=$(find "$DMGVolume" -name "*.app" -depth 1)
-
-    # Copy the contents of the DMG file to /Applications/
-    # Preserves all file attributes and ACLs
-    cp -pPR "$DMGAppPath" /Applications/
-
-    err=$?
-    if [ ${err} -ne 0 ]; then
-        echo "Could not copy $DMGAppPath Error: ${err}"
-        hdiutil detach $DMGMountPoint
-        echo "Used hdiutil to detach $DMGFile from $DMGMountPoint"
-        rm -r $Location
-        echo "Deleted $Location"
-        exit 1
-    fi
-
-    echo "Copied $DMGAppPath to /Applications"
-
-    # Unmount the DMG file
-    hdiutil detach $DMGMountPoint
-    echo "Used hdiutil to detach $DMGFile from $DMGMountPoint"
-
-    err=$?
-    if [ ${err} -ne 0 ]; then
-        abort "Could not detach DMG: $DMGMountPoint Error: ${err}"
-    fi
-
-    # Remove Temp Folder and download
+    #remove temp folder
     rm -r $Location
-    echo "Deleted $Location"
-       
-fi 
